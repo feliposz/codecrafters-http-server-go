1424b7d8d431b0ef1ac5b6f5aa623662fc273ca1
feliposz <feliposz@gmail.com>
2024-03-28 06:15:33 -0300

    Sending basic HTTP response

--- app/server.go
+++ app/server.go
@@ -20,10 +20,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = listener.Accept()
+	connection, err := listener.Accept()
+	defer connection.Close()
 	if err != nil {
-		fmt.Printf("Error accepting connection: %v", err)
+		fmt.Printf("Error accepting connection: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Client connected\n")
+
+	readBuffer := make([]byte, 2048)
+	bytesReceived, err := connection.Read(readBuffer)
+	if err != nil {
+		fmt.Printf("Error reading request: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Read %d bytes from client\n", bytesReceived)
+
+	httpResponse := "HTTP/1.1 200 OK\r\n\r\n"
+	bytesSent, err := connection.Write([]byte(httpResponse))
+	if err != nil {
+		fmt.Printf("Error sending response: %v\n", err)
 		os.Exit(1)
 	}
 
+	fmt.Printf("Sent %d bytes to client (expected: %d)\n", bytesSent, len(httpResponse))
 }
